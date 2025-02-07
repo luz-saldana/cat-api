02b870e6d425231a8ba52263ff4604efaa76c60d
luz-saldana <180133954+luz-saldana@users.noreply.github.com>
2025-02-07 01:02:33 -0800

    Added search cat by behavior feature

--- js/index.js
+++ js/index.js
@@ -1,17 +1,68 @@
-const projectSection = document.getElementById('cat-project');
-const projectList = projectSection.querySelector('ul');
+const url = `https://api.thecatapi.com/v1/breeds`;
+const api_key = 'live_wTn0o0wfYIBtbExUxfirZReRvfARViX7WSF1F6369C4g2Fuf5zdCazixwfjZvrhO';
 
-fetch('https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=live_wTn0o0wfYIBtbExUxfirZReRvfARViX7WSF1F6369C4g2Fuf5zdCazixwfjZvrhO').then((response) => {
+// a variable to store the breeds//
+let storedBreeds = [];
+let matchingBehavior = [];
+// let searchedBehavior = "";
+
+const searchForm = document.getElementById("searchFormId");
+
+searchForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    console.log(event);
+
+    const searchBehavior = document.getElementById("searchBehaviorId").value.toLowerCase();
+    if(!searchBehavior) {
+        alert("Please enter some text before searching.")
+        event.target.checked = false;
+        return;
+    }
+
+    for (let i = 0; i < storedBreeds.length; i++) {
+        const breed = storedBreeds[i];
+        const temperament = breed["temperament"].toLowerCase();
+        
+        console.log(temperament);
+        if (temperament.search(searchBehavior) > 0) {
+            matchingBehavior.push(breed);
+        }
+    }
+    console.log(matchingBehavior);
+
+    const randomInt = getRandomInteger(0, matchingBehavior.length -1);
+    const matchedBreed = matchingBehavior[randomInt];
+
+    catImgAndInfo(matchedBreed);
+
+})
+
+// //function to get a random breed//
+function getRandomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// //function to show images and information of the breeds//
+function catImgAndInfo(breedObject) {
+    // display image of cat//
+    document.getElementById("cat-image").src = breedObject.image.url;
+    //breed name//
+    document.getElementById("breed-name").innerHTML = breedObject.name;
+    //characteristics//
+    document.getElementById("breed-json").textContent = breedObject.temperament;
+}
+
+//fetch data//
+fetch(url, {
+    headers: {
+        "x-api-key": api_key,
+    },
+}).then((response) => {
+    if (!response.ok) {
+        throw new Error("Search failed");
+    }
     return response.json();
 }).then((data) => {
-    console.log("data: ", data);
-    for (let i = 0; i < data.length; i++) {
-        const project = data[i].breeds[0].name;
-        const li = document.createElement('li');
-        li.innerText = project;
-
-        projectList.appendChild(li);
-    }
-}).catch((error) => {
-    console.error("An error ocurred", error);
-});
+    storedBreeds = data;
+    console.log(storedBreeds);
+}).catch((error) => {console.error("An error ocurred",error)});
